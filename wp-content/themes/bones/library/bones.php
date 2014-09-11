ce03539e68ad506f34e5ef1bf7825993965e0b72
Wen Zhang <wen-ws317@mili-2.local>
2014-09-11 16:16:49 -0400

    new push

--- wp-content/themes/bones/library/bones.php
+++ wp-content/themes/bones/library/bones.php
@@ -149,7 +149,10 @@ function bones_scripts_and_styles() {
 		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
 
 		// jquery ui
-		wp_register_script( 'bones-jqueryui', get_stylesheet_directory_uri() . 'http://code.jquery.com/ui/1.11.1/jquery-ui.js', array('jquery'), true );
+		wp_register_script( 'bones-jqueryui', 'http://code.jquery.com/ui/1.11.1/jquery-ui.js', array('jquery'), true );
+
+		// waypoints
+		wp_register_script( 'bones-waypoint', get_stylesheet_directory_uri() . '/library/js/libs/waypoints.min.js', array(), '', true );
 
 		// enqueue styles and scripts
 		wp_enqueue_script( 'bones-modernizr' );
@@ -167,6 +170,7 @@ function bones_scripts_and_styles() {
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'bones-js' );
 		wp_enqueue_script( 'bones-jqueryui' );
+		wp_enqueue_script( 'bones-waypoint' );
 
 	}
 }
--- wp-content/themes/bones/library/js/scripts.js
+++ wp-content/themes/bones/library/js/scripts.js
@@ -113,6 +113,22 @@ jQuery(document).ready(function($) {
   */
   loadGravatars();
 
+  // Waypoints
+  // by default your element will be hidden
+  // $('.gamut-defined-wrapper').hide();
+  // // call waypoint plugin
+  // $('.gamut-defined-wrapper').waypoint(function(event, direction) {
+  //     // do your fade in here
+  //     $('.gamut-defined-wrapper').fadeIn();
+  // }, {
+  //    // offset: function() {
+  //    //     // The bottom of the element is in view
+  //    //     return $.waypoints('viewportHeight') - $(this).outerHeight();
+  //    //  }
+
+  //    offset: '75%'
+  // });
+
   // Debug code
    $('head').append('<style>#outline {position:fixed;z-index:1000;bottom:50px;right:50px;} .outlines {outline:1px solid rgba(255, 0, 0, 0.3);}</style>');
     $('body').append('<input id="outline" type="button">');
@@ -123,8 +139,8 @@ jQuery(document).ready(function($) {
 
   // Beam slide animation
   $('.home-beam').hide();
-  $('#rea-logo').click(function (){
-    $('.home-beam').show('slide', {direction:'right'}, 2000); 
+  $('#pro-logo').click(function (){
+    $('.home-beam').show('slide', {direction:'left'}, 200); 
   });
   
 
