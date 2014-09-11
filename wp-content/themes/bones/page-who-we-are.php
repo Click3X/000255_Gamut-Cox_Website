5a4394ea54133415059c4278d2e0759fd8ce20b0
Charles Howard Davis III <charles@click3x.com>
2014-09-11 17:51:25 -0400

    Added sql file

--- wp-content/themes/bones/page-who-we-are.php
+++ wp-content/themes/bones/page-who-we-are.php
@@ -16,22 +16,32 @@ $the_query = new WP_Query( $args );
 
 						<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
 
-							<?php if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
+							<?php if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); 
+
+								$emp_title = get_field('title');
+
+							?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-								<header class="article-header">
+									<?php
+									 		
+										$size = 'thumb';
+
+										if(the_post_thumbnail( $size )) {
+											the_post_thumbnail( $size );
+										}
+										?>
 
 									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
 
-									<!-- <p class="byline vcard">
-										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p> -->
+									<?php if($emp_title) {echo '<h2 class="team-member-title">'.$emp_title.'</h2>';} ?>
 
-								</header> <?php // end article header ?>
+								
 
 								<section class="entry-content cf" itemprop="articleBody">
 									<?php
+
 										// the content (pretty self explanatory huh)
 										the_content();
 
