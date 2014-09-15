2102057ae8c6f9ad8a72a9d3fc03006debc3ed43
Wen Zhang <wen-ws317@mili-2.local>
2014-09-15 17:59:51 -0400

    last push by the end today

--- wp-content/themes/bones/page-programatic.php
+++ wp-content/themes/bones/page-programatic.php
@@ -1,55 +1,132 @@
 <?php 
-/*
+ /* 
 
-Template Name: Programatic
+Template Name: Programmatic
 
-*/
+ */
+
+
+
 
 get_header(); ?>
 
+
+
 			<div id="content">
+			<?php
+				$header = get_field('header');
+				$main_sub_title = get_field('main-sub-title');
+				$main_first_p = get_field('main-first-p');
+				$main_p = get_field('main-p');
+
+				$lower_sub_title = get_field('lower-sub-title');
+				
+
+			?>
+				<header class="article-header">
+					<div class="page-header" style="background-image: url('<?php echo $header['url'] ?>')"></div>
+				</header>
 
 				<div id="inner-content" class="wrap cf">
 
-						<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+					<?php get_sidebar(); ?>						
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-
-								<header class="article-header">
-
-									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-
-								<!-- 	<p class="byline vcard">
-										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p> -->
-
-								</header> <?php // end article header ?>
+						
 
 								<section class="entry-content cf" itemprop="articleBody">
+
+
+									<div class="sub-page-content-wrapper">
+										<?php echo $main_sub_title 
+										// in back-end the title is wrapped with <h1> already
+										?> 
+										<div>
+											<!-- in back-end the title is wrapped with <p> already -->
+
+											<?php 
+
+											echo $main_first_p;
+											echo $main_p;
+
+											?>
+
+											
+
+										</div>
+
+
+									<!-- if statements -->
+									<!-- adding lower part on 4 product pages -->
+									<div class="lower-content">
+									<?php if (is_page(11)) { 
+										if(get_field('three-column-content')): ?>
+
+										<div class="lower-columns">
+
+											<h2><?php echo $lower_sub_title; ?></h2>
+											
+
+											<ul>
+
+											<?php while(has_sub_field('three-column-content')): ?>
+
+												<li>
+													<h3><?php the_sub_field('column-title'); ?></h3>
+													<p><?php the_sub_field('column-content'); ?></p>
+												</li>
+
+											<?php endwhile; ?>
+
+											</ul>
+
+										</div>
+
+										<?php endif; 
+
+										if (get_field('three-column-contact')):  ?>
+
+											<!-- <h2><?php echo $lower_sub_title; ?></h2> -->
+											
+
+											<ul class="lower-contact">
+
+											<?php while(has_sub_field('three-column-contact')): ?>
+
+												<li>
+													<h3><?php the_sub_field('column-department'); ?></h3>
+													<p><?php the_sub_field('column-person'); ?></p>
+												</li>
+
+											<?php endwhile; ?>
+
+											</ul>
+
+										<?php endif; ?>
+
+
+
+											<?php } elseif (is_page(8)) { ?>
+											   <!-- <image src="<?php bloginfo('stylesheet_directory'); ?>/images/imagename.jpg" /> -->
+											<?php } elseif (in_category( '5' )) { ?>
+											   <!-- <image src="<?php bloginfo('stylesheet_directory'); ?>/images/imagename.jpg" /> -->
+											<?php } ?>
+
+									</div> 
+									<!-- end of lower content -->
+
+
+
+
+									</div>
+									<!-- end of content wrapper -->
+
+
+									
+
 									<?php
-										// the content (pretty self explanatory huh)
-										// the_content();
 
-										$heading = get_field('heading');
-
-										$text = get_field('text_area');
-
-										$button = get_field('button');
-
-										$image = get_field('image');
-
-										echo '<h1>'.$heading.'</h1>';
-
-										echo '<div>'.$text.'</div>';
-
-										echo '<div>'.$button.'</div>';
-									?>
-
-										<img src="<?php echo $image ?>" />
-
-									<?php
 										/*
 										 * Link Pages is used in case you have posts that are set to break into
 										 * multiple pages. You can remove this if you don't plan on doing that.
@@ -73,30 +150,14 @@ get_header(); ?>
 
 								<footer class="article-footer cf">
 
-
 								</footer>
 
-							</article>
+								
 
-							<?php endwhile; else : ?>
-
-									<article id="post-not-found" class="hentry cf">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
 
 						</div>
 
-						<?php // get_sidebar(); ?>
+						
 
 				</div>
 
