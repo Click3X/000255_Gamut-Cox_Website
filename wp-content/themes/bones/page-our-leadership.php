3cd45d3f49fb7c0b485820a3b3cf2bdd1cc3d483
Charles Howard Davis III <charles@click3x.com>
2014-09-19 11:40:46 -0400

    ol 13

--- wp-content/themes/bones/page-our-leadership.php
+++ wp-content/themes/bones/page-our-leadership.php
@@ -12,13 +12,10 @@ if (have_posts()) : while (have_posts()) : the_post();
 	// GET HEADER
 	$header = get_field('header-image');
 	$tmpPosts = get_field('team_members');
-	
-	// helper($tmpPosts);
 
 	if( $tmpPosts ) {
-		// helper($tmpPosts[0]['team_member']);
+
 		$team_members = $tmpPosts[0]['team_member'];
-		// helper($team_members);
 
 		foreach ($team_members as $key => $team_member) {
 			helper($team_member);
@@ -44,74 +41,15 @@ if (have_posts()) : while (have_posts()) : the_post();
 			$teamMembers[$key]['largeThumb'] = $largeThumb[0];
 
 
-			echo '<h1>This is id: '.$teamMembers[$key]['id'].'</h1>';
-			echo '<h1>This is name: '.$teamMembers[$key]['name'].'</h1>';
-			echo '<h1>This is link: '.$teamMembers[$key]['link'].'</h1>';
-			echo '<h1>This is emp_title: '.$teamMembers[$key]['emp_title'].'</h1>';
-			echo '<h1>This is thumb: '.$teamMembers[$key]['thumb'].'</h1>';
-			echo '<h1>This is largeThumb: '.$teamMembers[$key]['largeThumb'].'</h1>';
-
-
-			// // CUSTOM FIELDS
-			// $emp_title = get_post_custom_values('title', $id)[0];
-
-			// // IMAGES
-			// $size = 'thumb';
-			// $full = 'large';
-			// $thumb = wp_get_attachment_image_src(get_post_thumbnail_id($id) , 'emp-thumb');
-			// $largeThumb = wp_get_attachment_image_src(get_post_thumbnail_id($id) , 'emp-large');
-
-			// //STORE VARS IN TEAM MEMBERS ARRAY
-			// $teamMembers[$key]['id'] = $id;
-			// // $teamMembers[$key]['content'] = $content;
-			// $teamMembers[$key]['name'] = $name;
-			// $teamMembers[$key]['link'] = $link;
-			// $teamMembers[$key]['emp_title'] = $emp_title;
-			// $teamMembers[$key]['thumb'] = $thumb;
-			// $teamMembers[$key]['large_thumb'] = $largeThumb;
+			// echo '<h1>This is id: '.$teamMembers[$key]['id'].'</h1>';
+			// echo '<h1>This is name: '.$teamMembers[$key]['name'].'</h1>';
+			// echo '<h1>This is link: '.$teamMembers[$key]['link'].'</h1>';
+			// echo '<h1>This is emp_title: '.$teamMembers[$key]['emp_title'].'</h1>';
+			// echo '<h1>This is thumb: '.$teamMembers[$key]['thumb'].'</h1>';
+			// echo '<h1>This is largeThumb: '.$teamMembers[$key]['largeThumb'].'</h1>';
 		}
 	}
 	helper($teamMembers);
-
-	// GET TEAM MEMBER POSTS RELATIONSHIP FIELD FROM 'OUR-LEADERSHIP' ADMIN PAGE
-	// if( $posts )
-	// 	{
-	// 		foreach( $posts as $post): // variable must be called $post (IMPORTANT);
-	//         setup_postdata($post); 
-	   		
-	//    		// $MEMBER HOLDS DATA
-
-	// 		$id = $post->ID;
-
-	// 		// CONTENT FIELDS
-	// 		// $content = $post->post_content;
-	// 		$name = $post->post_title;
-	// 		$link = $post->guid;
-
-	// 		// CUSTOM FIELDS
-	// 		$emp_title = get_post_custom_values('title', $id)[0];
-
-	// 		// IMAGES
-	// 		$size = 'thumb';
-	// 		$full = 'large';
-	// 		$thumb = wp_get_attachment_image_src(get_post_thumbnail_id($id) , 'emp-thumb');
-	// 		$largeThumb = wp_get_attachment_image_src(get_post_thumbnail_id($id) , 'emp-large');
-
-	// 		//STORE VARS IN TEAM MEMBERS ARRAY
-	// 		$teamMembers[$key]['id'] = $id;
-	// 		// $teamMembers[$key]['content'] = $content;
-	// 		$teamMembers[$key]['name'] = $name;
-	// 		$teamMembers[$key]['link'] = $link;
-	// 		$teamMembers[$key]['emp_title'] = $emp_title;
-	// 		$teamMembers[$key]['thumb'] = $thumb;
-	// 		$teamMembers[$key]['large_thumb'] = $largeThumb;
-
-
-	//    		endforeach;
-	    
-	//     wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly 
-		
-	// 	}
 
 	endwhile;
 
@@ -135,6 +73,22 @@ endif;
 
 		<ul id="og-grid" class="og-grid">
 
+			<?php 
+
+				foreach ($teamMembers as $key => $teamMember) { ?>
+					<li id="post-<?php echo $teamMember['id']; ?>" role="article" itemscope itemtype="http://schema.org/BlogPosting">
+						<a href="<?php echo $teamMember['link']; ?>" data-largesrc="<?php echo $teamMember['largeThumb']; ?>" data-title="<?php echo $teamMember['name']; ?>" data-description="<?php echo $teamMember['content']; ?>" data-etitle="<?php echo $teamMember['emp_title']; ?>">
+						<?php echo '<a href="'.$teamMember['link'].'">';
+							echo '<img src="'.$teamMember['thumb'].'" alt="'.$teamMember['name'].'" class="grid-thumb">';
+							echo '<h2 class="small-name gothic">'.$teamMember['name'].'</h2>';
+							echo '<h3 class="small-title gothic">'.$teamMember['emp_title'].'</h3>';
+							?>
+						</a>
+					</li>
+				<?php
+
+				}
+			?>
 
 		</ul>
 
