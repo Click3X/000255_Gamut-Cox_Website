5d172170cf1c829eccea682b882d993c62d94398
Charles Howard Davis III <charles@click3x.com>
2014-09-25 19:11:51 -0400

    Merging for staging push

--- wp-content/themes/bones/page-dev-thursday-test.php
+++ wp-content/themes/bones/page-dev-thursday-test.php
@@ -1,3 +1,18 @@
+<?php
+/*
+ Template Name: Home
+ *
+ * This is your custom page template. You can create as many of these as you need.
+ * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * top, the same way it is here.
+ *
+ * When you create your page, you can just select the template and viola, you have
+ * a custom page template to call your very own. Your mother would be so proud.
+ *
+ * For more info: http://codex.wordpress.org/Page_Templates
+*/
+?>
+
 <?php get_header(); ?>
 
 <?php 
@@ -52,10 +67,10 @@ if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->t
 	endif;
 
 	// PARALLAX FILE
-//	include('php/para-new-new-mobile.php'); 
+	// include('php/para-friday.php'); 
 
-	// PARALAXX SCROLL MAGIC
-	include('php/para-kieth.php');
+	// PARALLAX FILE
+	include('php/para-tues.php'); 
 
 ?>	
 
