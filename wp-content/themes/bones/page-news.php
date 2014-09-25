65ec358f2b3dfca0970111a9721b4a9ed3536e7b
Wen Zhang <wen-ws317@mili-2.local>
2014-09-24 23:08:56 -0400

    events half done

--- wp-content/themes/bones/page-news.php
+++ wp-content/themes/bones/page-news.php
@@ -61,7 +61,7 @@
 
 										<h1 class="post-header"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 										<p class="byline vcard">
-											<?php printf( __( '<time class="updated" datetime="%1$s" pubdate>%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format'))); ?>
+											<?php //printf( __( '<time class="updated" datetime="%1$s" pubdate>%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format'))); ?>
 										</p>
 
 
